4b5b3ef33349a66f28a27018203c0fc6fd0ff796
Nayyum <Navyum12@gmail.com>
2024-03-17 11:35:52 +0000

    Conversion of Data type

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33ac";
+
+// const {score} = req.body
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);     // NaN 
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1;  false => 0
+
+let isLoggedIn = "";
+
+let booleanIsLoggedin = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedin);
+
+// 1 => true; 0 => false
+// "" => false
+// "navyum" => true
+
+
+let someNUmber = 0
+
+let stringNumber = String(someNUmber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
