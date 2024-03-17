01237d6b49ecc3710a9c12b96e680fb5ba3f5caf
Nayyum <Navyum12@gmail.com>
2024-03-17 08:24:34 +0000

    datTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 2506;
+let accountEmail = "navyum2@gmail.com";
+var accountPassword = "52678";
+accountcity = "haryana";
+let accountState;
+
+
+accountEmail = "hmm@gmail.com";
+accountPassword = "7858";
+accountcity = "panipat"
+
+
+console.log(accountId);
+/*
+perfer not to use var
+because of issue in block and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState]);
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";   // treat all JS code as newer version
+
+// alert ( 3 + 3 )  // we are using nodejs, not browser
+
+// console.log(3+3)    console.log("Navyum");  // code readability should be high
+
+let name = "navyum"
+let age = 18
+let isLoggedIn = false;
+
+// number -> 2 to power 53
+// bigint
+// string -> '', "", ``
+// boolean -> true/false
+// null -> standalone value
+// undefined ->
+// symbol -> uniqueness ke liye use hota hai mostly in react and figma
+
+// object 
+
+console.log(typeof "navyum")    // string
+console.log(typeof undefined)   // undefined
+console.log(typeof null)        // object
