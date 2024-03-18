d4634c45972890de121126ee3128428fc8b8b461
Nayyum <Navyum12@gmail.com>
2024-03-18 13:42:31 +0000

    memories

--- 01_basic/datatypes.summary.js
+++ 01_basic/datatypes.summary.js
@@ -35,3 +35,33 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (non-Primitive)
+
+let myYoutubename = "Navyum";
+
+let anothername = myYoutubename
+anothername = "Ansari"
+
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "navyum@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
